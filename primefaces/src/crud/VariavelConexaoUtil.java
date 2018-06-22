b0468dbfe12d17a47a328548de85bbb14abd7eac
Cristiano <Cristiano@DESKTOP-C2NKC8T>
2018-06-21 21:36:18 -0300

    Primeiro commit

---
+++ primefaces/src/crud/VariavelConexaoUtil.java
@@ -0,0 +1,13 @@
+package crud;
+
+/**
+ * Caminho do JNDI datasource tomcat
+ * @author Cristiano
+ * 
+ */
+
+public class VariavelConexaoUtil {
+
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+	
+}
